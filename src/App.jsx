b04e109fb675f7d9a458f267ca972a5b76799518
Carol-MS2001 <cintiacmartelsanchez7@gmail.com>
2025-04-25 22:32:07 -0500

    Primer commit: Lista de tareas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>✨ Mi App React con To-Do List ✨</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
